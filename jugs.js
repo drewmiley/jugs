704cf91d50d60333013843357da67c5dcf52140b
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-19 12:52:59 +0000

    Implements solution

--- jugs.js
+++ jugs.js
@@ -1,14 +1,33 @@
 const euclid = (a, b) => {
-    console.log(a);
-    console.log(b);
-    return [1, 1];
+    let aCopy = a;
+    let bCopy = b;
+    let aComb = [1, 0];
+    let bComb = [0, 1];
+    do {
+        if (aCopy >= bCopy) {
+            const remainder = aCopy % bCopy;
+            const mult = (aCopy - remainder) / bCopy;
+            aCopy -= mult * bCopy;
+            aComb = [
+                aComb[0] - mult * bComb[0],
+                aComb[1] - mult * bComb[1]
+            ];
+        } else {
+            const remainder = bCopy % aCopy;
+            const mult = (bCopy - remainder) / aCopy;
+            bCopy -= mult * aCopy;
+            bComb = [
+                bComb[0] - mult * aComb[0],
+                bComb[1] - mult * aComb[1]
+            ];
+        }
+    } while (aCopy % bCopy || bCopy % aCopy);
+    return aCopy == 0 ? bComb : aComb;
 }
 
 const hcf = (a, b) => {
     const euclidMultipliers = euclid(a, b);
-    const hcf = euclidMultipliers[0] * a + euclidMultipliers[1] * b;
-    console.log(hcf);
-    return 1;
+    return euclidMultipliers[0] * a + euclidMultipliers[1] * b;
 }
 
 const solveJugs = (jug1, jug2, fill) => {
