81d7b5c82d6d92e50ae2ed7baa8bb513c7180014
Drew Miley <drew.miley@opencastsoftware.com>
2019-03-19 11:19:24 +0000

    Error handling displays

--- jugs.js
+++ jugs.js
@@ -3,20 +3,22 @@ const hcf = (a, b) => {
 }
 
 const solveJugs = (jug1, jug2, fill) => {
-    console.log("Jugs");
+    output.innerHTML = "Jugs";
 }
 
 (function () {
     document.getElementById("solve").onclick = () => {
+        const output = document.getElementById("output");
+        output.innerHTML = "";
         const jug1 = parseInt(document.getElementById("jug1").value, 10);
         const jug2 = parseInt(document.getElementById("jug2").value, 10);
         const fill = parseInt(document.getElementById("fill").value, 10);
         if (isNaN(jug1 + jug2 + fill)) {
-            console.log("Invalid Input");
+            output.innerHTML = "Invalid Input";
         } else if (jug1 <= 0 || jug2 <= 0 || fill <= 0) {
-            console.log("Inputs should be greater than 0");
+            output.innerHTML = "Inputs should be greater than 0";
         } else if (hcf(jug1, jug2) > 1) {
-            console.log("Jug levels should be coprime");
+            output.innerHTML = "Jug levels should be coprime";
         } else {
             solveJugs(jug1, jug2, fill);
         }
